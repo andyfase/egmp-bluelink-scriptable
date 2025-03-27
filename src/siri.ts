f97efc68c32c210b94f40d909ed4339b9bb22584
Andy Fase <andrewfase@gmail.com>
2025-03-27 16:17:50 -0700

    Siri - add delay to allow for re-auth commands before returning and script completion

--- src/siri.ts
+++ src/siri.ts
@@ -65,9 +65,9 @@ async function getStatus(bl: Bluelink): Promise<string> {
 
 async function getRemoteStatus(bl: Bluelink): Promise<string> {
   // send remote status request but dont wait for response as it takes to long
-  // wait 500ms just to ensure we sent the command
+  // wait 5000ms just to ensure we send the command and allow for re-auth etc to complete
   bl.getStatus(true, true)
-  await sleep(500)
+  await sleep(3000)
   return "I've issued a remote status request. Ask me for the normal status again in 30 seconds and I will have your answer."
 }
 
@@ -171,12 +171,18 @@ async function stopCharge(bl: Bluelink): Promise<string> {
 async function blRequest(bl: Bluelink, type: string, message: string, payload?: any): Promise<string> {
   let gotFirstCallback = false
   let counter = 1
+  const start = Date.now()
   bl.processRequest(type, payload, async (_isComplete, _didSucceed, _data) => {
     gotFirstCallback = true
   })
   while (!gotFirstCallback && counter < 10) {
     await sleep(500)
     counter += 1
+  }
+  const duration = Date.now() - start
+  if (duration < 3000) { 
+    // sleep until 3 seconds have passed to allow for any re-auth etc commands to complete
+    await sleep(3000 - duration)
   }
   return message
 }
