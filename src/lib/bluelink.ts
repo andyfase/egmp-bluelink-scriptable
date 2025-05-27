5a3265c8611bd30e7546b36c5a9c4a8ccc3e5862
samay <samay@electron.in>
2025-05-27 22:08:55 +0530

    Added support for Hyundai in India region.

--- src/lib/bluelink.ts
+++ src/lib/bluelink.ts
@@ -4,6 +4,7 @@ import { BluelinkCanada } from './bluelink-regions/canada'
 import { BluelinkUSA } from './bluelink-regions/usa'
 import { BluelinkUSAKia } from './bluelink-regions/usa-kia'
 import { BluelinkEurope } from './bluelink-regions/europe'
+import { BluelinkIndia } from './bluelink-regions/india'
 
 export async function initRegionalBluelink(config: Config): Promise<BluelinkCanada | Bluelink | undefined> {
   switch (config.auth.region) {
@@ -13,6 +14,8 @@ export async function initRegionalBluelink(config: Config): Promise<BluelinkCana
       return config.manufacturer === 'hyundai' ? await BluelinkUSA.init(config) : await BluelinkUSAKia.init(config)
     case 'europe':
       return await BluelinkEurope.init(config)
+    case 'india':
+      return await BluelinkIndia.init(config)
     default:
       return
   }
